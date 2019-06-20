4b046b6bed2243b0143a097293e9112d20a26776
Reza Esmaili <bitbucket@dfox.info>
2019-06-20 19:50:26 +0200

    Remove vue reference

--- resources/views/grid/entry_name.blade.php
+++ resources/views/grid/entry_name.blade.php
@@ -1,4 +1,4 @@
-@if ($record->is_remote)<span class="badge badge-danger" v-if="is_remote">REMOTE</span> @endif<a href="" class="show-entry-description" data-id="{{$record->id}}">{{$record->title}}</a>
+@if ($record->is_remote)<span class="badge badge-danger">REMOTE</span> @endif<a href="" class="show-entry-description" data-id="{{$record->id}}">{{$record->title}}</a>
 <br>
 by {{$record->author}}
 
