fb60f113921876e164f146b43162f0ca16bdcbb7
Reza Esmaili <github@dfox.info>
2020-07-12 16:44:33 +0200

    Fix package link

--- resources/assets/js/partymeister-competitions/components/EntryModal.vue
+++ resources/assets/js/partymeister-competitions/components/EntryModal.vue
@@ -269,8 +269,8 @@
 </style>
 
 <script>
-    import {Ziggy} from 'ziggy';
-    import route from 'ziggy/src/js/route';
+    import {Ziggy} from 'ziggy-js';
+    import route from 'ziggy-js/src/js/route';
 
     window.Ziggy = Ziggy; // this was missing from your setup
 
